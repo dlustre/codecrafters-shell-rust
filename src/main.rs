bd2a52e378303f406cb153e62bd0be878963ff36
dlustre <dennis.lustre@gmail.com>
2024-06-20 00:53:52 -0700

    add echo

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     process::exit,
 };
 
+// TODO: add struct / enum for commands?
+
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -12,6 +14,7 @@ fn main() {
     let mut input = String::new();
     while let Ok(_num_bytes) = stdin.read_line(&mut input) {
         match input.trim().split_whitespace().collect::<Vec<&str>>()[..] {
+            ["echo", ref args @ ..] => println!("{}", args.join(" ")),
             ["exit", code] => exit(code.parse::<i32>().unwrap()),
             [unknown_command, ..] => println!("{unknown_command}: command not found"),
             _ => {}
