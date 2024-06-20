0456b789f7c9a49ef393f85852eba6a89c01ea09
dlustre <dennis.lustre@gmail.com>
2024-06-20 00:24:23 -0700

    add repl

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ fn main() {
     while let Ok(_num_bytes) = stdin.read_line(&mut input) {
         let formatted = input.trim();
         println!("{formatted}: command not found");
+        print!("$ ");
         io::stdout().flush().unwrap();
+        input.clear();
     }
 }
