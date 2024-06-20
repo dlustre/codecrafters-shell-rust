13fca13ce0585fbcbf4e2d7137bd9090613e8a85
dlustre <dennis.lustre@gmail.com>
2024-06-20 02:46:42 -0700

    implement type command

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,38 @@ use std::{
     process::exit,
 };
 
-// TODO: add struct / enum for commands?
-
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     while let Ok(_num_bytes) = stdin.read_line(&mut input) {
-        match input.trim().split_whitespace().collect::<Vec<&str>>()[..] {
-            ["echo", ref args @ ..] => println!("{}", args.join(" ")),
-            ["exit", code] => exit(code.parse::<i32>().unwrap()),
-            [unknown_command, ..] => println!("{unknown_command}: command not found"),
-            _ => {}
+        if let Some((command, params)) = input
+            .trim()
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .split_first()
+        {
+            match *command {
+                "type" => {
+                    for param in params {
+                        match *param {
+                            "type" | "exit" | "echo" => println!("{param} is a shell builtin"),
+                            _ => println!("{param}: not found"),
+                        }
+                    }
+                }
+                "exit" => match params[..] {
+                    [code, ..] => match code.parse::<i32>() {
+                        Ok(code) => exit(code),
+                        Err(_) => println!("Invalid exit code"),
+                    },
+                    [] => println!("No error code provided"),
+                },
+                "echo" => println!("{}", params.join(" ")),
+                _ => println!("{command}: not found"),
+            }
         }
         print!("$ ");
         io::stdout().flush().unwrap();
